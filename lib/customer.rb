632d16b1a29d734a035f28e084a2bb9361861883
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-12-06 21:51:20 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+#self.new_meal?
+  def new_meal(waiter, total, tip = 0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|eats| eats.customer == self}
+#binding.pry  
+end
+
+  def waiters
+    meals.map {|my_meal| my_meal.waiter}
+  end 
+#who's Terrance's last waiter?
+#terrance.waiters.last.name 
+    #=> "Yomi" 
 end
